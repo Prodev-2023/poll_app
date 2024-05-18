2b87e108e6f9ccab0f8e9048228021ec2fae824b
prodev-2023 <protech2023.32@gmail.com>
2024-05-18 06:08:16 +0100

    class base view used

--- polls/urls.py
+++ polls/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path
 from polls import views
 
+# app_name = "polls"
+# urlpatterns = [
+#     #ex: /polls/
+#     path('', views.index, name='index-page'),
+#     #ex: /polls/5/
+#     path("<int:question_id>/", views.detail, name='detail-page'),
+#     #ex: /polls/5/results/
+#     path("<int:question_id>/results/", views.results, name='results-page'),
+#     #ex: /polls/5/vote/
+#     path("<int:question_id>/vote/", views.vote, name='vote-page'),
+# ]
+
+
 app_name = "polls"
 urlpatterns = [
-    #ex: /polls/
-    path('', views.index, name='index-page'),
-    #ex: /polls/5/
-    path("<int:question_id>/", views.detail, name='detail-page'),
-    #ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name='results-page'),
-    #ex: /polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name='vote-page'),
+    path("", views.IndexView.as_view(), name="index-page"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail-page"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results-page"),
+    path("<int:question_id>/vote/", views.vote, name="vote-page"),
 ]
